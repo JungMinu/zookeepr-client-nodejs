eedaf7cd64200512e15db3faccf8933c7a194d01
Minwoo Jung <jmwsoft@gmail.com>
2015-09-08 00:26:45 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,46 +1,22 @@
-var async = require('async');
-var exec = require('child_process').exec;
+var zooArray = [
+        {
+            "host": "localhost:2181",
+            "path": "./zookeeper_cluster/zookeeper1/zookeeper-3.4.6/bin/"
+        },
+        {
+            "host": "localhost:2182",
+            "path": "./zookeeper_cluster/zookeeper2/zookeeper-3.4.6/bin/"
+        },
+        {
+            "host": "localhost:2183",
+            "path": "./zookeeper_cluster/zookeeper3/zookeeper-3.4.6/bin/"
+        }
+    ];
 
-var gk = require("./common");
-var config = gk.config;
-var smLauncher = gk.smLauncher;
-var smStateWatcher = gk.smStateWatcher.smStateWatcher;
-var smStateEventHandler = gk.smStateWatcher.EventHandler;
+var zoonum = zooArray.length;
 
-var smArray = config.smArray;
-
-var zooConfig = config.zooConfig;
-var zoosmPath = zooConfig.smPath;
-var zooArray = config.zooArray;
-var QuorumNum = zooArray.length;
-
-var zooHost = zooArray[0].host;
-
-for (var i = 1; i < zooArray.length; i++) {
-    zooHost = zooHost + "," + zooArray[i].host;
-}
-
-var EventEmitter = require('events').EventEmitter;
-var zooServesmtart = new EventEmitter();
-var MongoStart = new EventEmitter();
-
-zooServesmtart.on('start', function() {
-    for (var i = 0; i < QuorumNum; i++) {
-        exec("sudo " + zooArray[i].path + "zooServer.sh start", function(error, stdout, stderr) {
+for (var i = 0; i < zoonum; i++) {
+        exec("sudo " + zooArray[i].path + "zkServer.sh start", function(error, stdout, stderr) {
             console.log(stdout);
         });
-    }
-});
-
-// Zookeeper cluster server 실행
-async.series([    
-    function asynczooServesmtart(cb) {
-        zooServesmtart.emit('start');
-        cb(null, "zooServer start");
-    },
-    
-], function done(error, results) {
-    console.log('err: ', error);
-    smStateWatcher.start(smArray, smStateEventHandler, MongoConfig, zoosmPath, zooHost);
-
-});
+}
