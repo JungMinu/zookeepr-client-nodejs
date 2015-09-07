51040cffdb511323e5cbd23d47a2dac3c25e7df7
Minwoo Jung <jmwsoft@gmail.com>
2015-09-07 19:30:29 +0900

    Update Watcher.js

--- zookeeper_watcher/Watcher.js
+++ zookeeper_watcher/Watcher.js
@@ -12,7 +12,6 @@ function JSONtoString(object) {
     return '{' + results.join(', ') + '}';
 }
 
-// 만약 특정 mongod에 해당하는 Ephemeral Node가 변경될 경우, change event에 따라 적절한 handler에게 전달해 rs 상태를 zkServer에 다시 갱신.
 function Dispatcher(zkClient, rsPort, NodeCreatedEventHandler, NodeDeletedEventHandler, znode, zkRsPath, isStoreState) {
     zkClient.exists(
     znode,
@@ -38,13 +37,7 @@ function Dispatcher(zkClient, rsPort, NodeCreatedEventHandler, NodeDeletedEventH
             console.log(error.stack);
         }
 
-        // 만약 와쳐를 걸어 둔 Node가 존재하고(stat) 'NODE_CREATED' event.type이었다면(isStoreState)
-        // 몽고가 처음 시작된 경우이거나
-        // 몽고가 죽었다가 살아난 경우이다.
-        // 즉 NodeCreatedEventHandler에게 전달해 rs 상태를 갱신한다.
-        if(stat && isStoreState) {
-            NodeCreatedEventHandler.start(zkClient, rsPort, znode, zkRsPath);
-        }
+        
     });
 } 
 
